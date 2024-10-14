e76f6852b2a56cd477b7c310d8e3c69a977c6611
GODIN Nicolas <nicolas.godin@sully-group.fr>
2024-10-14 09:10:33 +0200

    change title

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,7 @@
 <template>
   <main>
     <div class="">
-      <h1 class="text-2xl text-center">Niko Armes</h1>
+      <h1 class="text-2xl text-center">Gears of shoot</h1>
     </div>
   </main>
 </template>
